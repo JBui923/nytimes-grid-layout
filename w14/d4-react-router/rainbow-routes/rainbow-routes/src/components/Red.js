83266cbec348e705d0e0c60ac3a57c037fbb5738
Johnny <jbui923@gmail.com>
2020-05-28 10:10:52 -0700

    rainbow road

---
+++ w14/d4-react-router/rainbow-routes/rainbow-routes/src/components/Red.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import Orange from './Orange';
+import Yellow from './Yellow';
+
+// const Red = () => (
+//   <div>
+//     <h2 className="red">Red</h2>
+//     <NavLink exact to='/red'>Red only</NavLink>
+//     <NavLink to='/red/yellow'>Add yellow</NavLink>
+//     <NavLink to='/red/orange'>Add orange</NavLink>
+
+//     <Route path="/red/orange" component={Orange} />
+//     <Route path="/red/yellow" component={Yellow} />
+//   </div>
+// );
+
+class Red extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2 className="red">Red</h2>
+        <NavLink exact to='/red'>Red only</NavLink>
+        <NavLink to='/red/yellow'>Add yellow</NavLink>
+        <NavLink to='/red/orange'>Add orange</NavLink>
+
+        <Route path="/red/orange" component={Orange} />
+        <Route path="/red/yellow" component={Yellow} />
+      </div>
+    );
+  }
+};
+
+export default Red;
